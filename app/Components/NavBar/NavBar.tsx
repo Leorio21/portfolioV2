f1b69cdc0b05a17b593b078a7e7a084a9e3bcbdd
Leorio21 <Ptitlue@gmail.com>
2023-10-01 21:31:28 +0200

    Fix: retrait de code inutile

--- app/Components/NavBar/NavBar.tsx
+++ app/Components/NavBar/NavBar.tsx
@@ -1,11 +1,8 @@
-"use client";
 import React, { useEffect, useState } from "react";
 import styles from "./NavBar.module.css";
 import ToggleTheme from "../ToggleTheme/ToggleTheme";
 
 export default function NavBar(): JSX.Element {
-  const [toggleTheme, setToggleTheme] = useState(false);
-  const [buttonText, setButtonText] = useState("Light");
 
   return (
     <div className={styles.container}>
