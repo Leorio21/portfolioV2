ef23e0616d1f423ab80c2eabd2e87a674a922d79
Leorio21 <Ptitlue@gmail.com>
2023-10-08 11:52:15 +0200

    Style: project card

--- app/Components/Projets/ProjetCard.tsx
+++ app/Components/Projets/ProjetCard.tsx
@@ -13,11 +13,25 @@ export default function ProjetCard({ projet, nmr }: ProjetCardProps) {
     <article
       className={
         nmr % 2 === 0
-          ? `${styles.container} ${styles.miniatureLeft}`
-          : `${styles.container} ${styles.miniatureRight}`
+          ? styles.container
+          : `${styles.container} ${styles.reverse}`
       }
       id={projet.title}
     >
+      <div className={styles.tilt}>
+        <svg
+          className={nmr % 2 !== 0 ? `${styles["tilt-rotate"]}` : ""}
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M1200 120L0 16.48 0 0 1200 0 1200 120z"
+            className={styles["shape-fill"]}
+          ></path>
+        </svg>
+      </div>
       <figure className={styles.miniature}>
         <Image
           src={projet.miniature}
@@ -29,22 +43,24 @@ export default function ProjetCard({ projet, nmr }: ProjetCardProps) {
           fill
         />
       </figure>
-      <h3 className={styles.title}>{projet.title}</h3>
-      <p className={styles.description}>{projet.description}</p>
-      {projet.code !== "" && (
-        <p className={`${styles.code} ${styles.lien}`}>
-          <a href={projet.code} target="_blank" rel="noreferrer">
-            Voir le code
-          </a>
-        </p>
-      )}
-      {projet.site !== "" && (
-        <p className={`${styles.site} ${styles.lien}`}>
-          <a href={projet.site} target="_blank" rel="noreferrer">
-            Voir le site
-          </a>
-        </p>
-      )}
+      <div className={styles["project-infos"]}>
+        <h3 className={styles.title}>{projet.title}</h3>
+        <p className={styles.description}>{projet.description}</p>
+        {projet.code !== "" && (
+          <p className={`${styles.code} ${styles.lien}`}>
+            <a href={projet.code} target="_blank" rel="noreferrer">
+              Voir le code
+            </a>
+          </p>
+        )}
+        {projet.site !== "" && (
+          <p className={`${styles.site} ${styles.lien}`}>
+            <a href={projet.site} target="_blank" rel="noreferrer">
+              Voir le site
+            </a>
+          </p>
+        )}
+      </div>
     </article>
   );
 }
